4706f5086d6292c01bdc5e8d355366569c516c29
sergei khvalko <falko.serega2016@yandex.ru>
2024-07-07 18:47:34 +0300

    refactor: change img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import SearchBar from './components/searchBar/SearchBar'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +21,7 @@ export default function RootLayout({
 			<body className={inter.className}>
 				<div className="relative bg-black">
 					<div className="absolute top-0 left-0 w-full h-full">
-						<img
+						<Image
 							src="/background.png"
 							alt="backgroundImage"
 							className="h-screen w-full object-cover"
